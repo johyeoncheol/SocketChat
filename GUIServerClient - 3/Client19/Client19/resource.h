a4778f953ca64a78fd798b0bf6bf67a64ece5e03
unknown <qweas46@naver.com>
2020-06-29 22:06:08 +0900

    room info

---
+++ GUIServerClient - 3/Client19/Client19/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// GUIApp.rc에서 사용되고 있습니다.
+//
+#define IDD_DIALOG1                     101
+#define IDC_LIST                        1001
+#define IDC_EDIT1                       1002
+#define IDC_EDIT                        1002
+#define IDC_SEND                        1003
+#define IDC_BUTTON1                     1004
+#define IDC_OPERATOR                    1004
+#define IDC_X                           1005
+#define IDC_Y                           1006
+#define IDC_F                           1007
+#define IDC_RADIO1                      1008
+#define IDC_RADIO2                      1009
+#define IDC_RADIO3                      1010
+#define IDC_RADIO4                      1011
+#define IDC_DISPLAY                     1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
